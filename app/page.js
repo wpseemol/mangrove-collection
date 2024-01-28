9ce55bead59e3c813695d13c7add614f7e44cc00
wpseemol <wpseemol@gmail.com>
2024-01-28 21:39:56 +0600

    install next js for fish tower project giterda

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Footer from './footer';
+import Header from './header';
+import Main from './main';
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
