b82ae1c094a2c73013b59745a0d8b006c810918f
wpseemol <wpseemol@gmail.com>
2024-03-02 21:52:39 +0600

    main and footer component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
+import Footer from '../../Components/Footer/Footer';
+import Main from '../../Components/Main/Main';
 import Nav from '../../Components/NavBar/Nav/Nav';
 
 export default function Home() {
     return (
         <>
             <Nav />
+
+            <Main />
+
+            <Footer />
         </>
     );
 }
