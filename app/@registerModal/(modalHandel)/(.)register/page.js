b3258d73d885d1efd6e27dc822a932e25e977ac7
wpseemol <wpseemol@gmail.com>
2024-04-26 23:52:32 +0600

    intercept problem solve

--- app/@registerModal/(modalHandel)/(.)register/page.js
+++ app/@registerModal/(modalHandel)/(.)register/page.js
@@ -1,18 +1,3 @@
-import {
-    InterceptClosBtn,
-    InterceptClosBtnOutSite,
-} from '@/app/Components/Client/InterceptClosBtn/InterceptClosBtn';
-import LoginFrom from '@/app/Components/LoginFrom/loginFrom';
-
 export default function RegisterIntercept() {
-    return (
-        <section className="fixed top-0 left-0 z-40 w-screen h-screen bg-slate-700/65 flex items-center justify-center">
-            <InterceptClosBtnOutSite />
-            <div className=" relative w-fit h-fit mx-auto z-20 ">
-                <InterceptClosBtn />
-
-                <LoginFrom where="intercept" />
-            </div>
-        </section>
-    );
+    return <></>;
 }
