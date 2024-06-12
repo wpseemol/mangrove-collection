a0f85c465c8f5ee7a4f070a85f4eef0140ce821d
wpseemol <wpseemol@gmail.com>
2024-06-12 21:06:28 +0600

    popen and roboto font add on project

---
+++ app/Components/_home/PopularProducts.jsx
@@ -0,0 +1,11 @@
+import HomeTitle from '@app/Components/HomeTitle/HomeTitle';
+
+export default function PopularProducts() {
+    return (
+        <section className="container mx-auto mb-10">
+            <HomeTitle>
+                <samp className="uppercase">POPULAR PRODUCTS 🔥 </samp>
+            </HomeTitle>
+        </section>
+    );
+}
