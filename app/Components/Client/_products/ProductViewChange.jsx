049b3c3f913d389642895d548f28f955caefc132
wpseemol <wpseemol@gmail.com>
2024-06-23 23:58:15 +0600

    card desigine deone

--- app/Components/Client/_products/ProductViewChange.jsx
+++ app/Components/Client/_products/ProductViewChange.jsx
@@ -1,24 +1,38 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaListUl } from 'react-icons/fa6';
 import { SlGrid } from 'react-icons/sl';
 
 export default function ProductViewChange({ children, ListViewCard }) {
     const [isGridView, setIsGridView] = useState('gird');
 
+    useEffect(() => {
+        const isCardView = sessionStorage.getItem('card-view');
+
+        if (isCardView) {
+            setIsGridView(isCardView);
+        }
+    }, []);
+
     return (
         <div className="">
             <div className="border-b w-full flex gap-2 items-center pb-3">
                 <button
                     title="Grid view"
-                    onClick={() => setIsGridView('gird')}
+                    onClick={() => {
+                        setIsGridView('gird');
+                        sessionStorage.removeItem('card-view');
+                    }}
                     className="border p-2 text-nowrap rounded-sm">
                     <SlGrid />
                 </button>
                 <button
                     title="List view"
-                    onClick={() => setIsGridView('list')}
+                    onClick={() => {
+                        setIsGridView('list');
+                        sessionStorage.setItem('card-view', 'list');
+                    }}
                     className="border p-2 text-nowrap rounded-sm">
                     <FaListUl />
                 </button>
--- app/Components/_card/ListViewCard.jsx
+++ app/Components/_card/ListViewCard.jsx
@@ -1,5 +1,6 @@
 import currencyIcon from '@/utils/currencyIcon';
 import Image from 'next/image';
+import CardBtn from './CardBtn';
 
 export default function ListViewCard({ productDetails }) {
     const {
@@ -15,8 +16,11 @@ export default function ListViewCard({ productDetails }) {
     } = productDetails;
 
     return (
-        <div className="w-full border border-neutral-300/30 grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 items-center gap-4 ">
-            <figure className="border col-span-1 h-[17.5rem] overflow-hidden flex justify-center items-center">
+        <div
+            className="w-full shadow hover:shadow-lg grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 items-center gap-4 lg:h-[17.5rem] md:h-[12rem] h-[17.5rem]
+        duration-200
+        ">
+            <figure className="animate-fade animate-duration-1000 col-span-1 w-full h-full overflow-hidden flex justify-center items-center border-r">
                 <Image
                     src={thumbnail}
                     width={300}
@@ -25,22 +29,21 @@ export default function ListViewCard({ productDetails }) {
                     alt={productName}
                 />
             </figure>
-            <div className=" lg:col-span-3 sm:col-span-2 border w-full">
-                <p>{category?.name}</p>
-                <h2>{productName}</h2>
-                <p className="animate-fade-up animate-duration-1000">
+            <div className=" animate-fade-left animate-duration-1000 md:col-span-3 sm:col-span-2  w-full">
+                <p className="text-xs font-extralight hover:text-primaryColor duration-200 text-[#999]">
+                    {category?.name}
+                </p>
+                <h2 className="text-[28px] font-medium my-2">{productName}</h2>
+                <p className=" text-green-800 mb-2">
                     <span>{currencyIcon(currency)}</span> {price}{' '}
                     <span className="font-semibold">(1{unit})</span>
                 </p>
-                <p>{shortDescription}</p>
+                <p className="text-[#585858] text-base mb-2">
+                    {shortDescription}
+                </p>
 
-                <div>
-                    <button className="text-sm px-4 py-2 bg-green-600 border border-green-600 rounded text-neutral-200 font-bold hover:text-white hover:bg-green-500 origin-top duration-500">
-                        Buy Now
-                    </button>
-                    <button className=" text-sm px-3 py-2 bg-neutral-200 border border-neutral-200 rounded text-green-700 font-bold hover:text-green-600 hover:bg-white origin-center duration-300">
-                        Add to Cart
-                    </button>
+                <div className="flex items-center gap-3 ">
+                    <CardBtn />
                 </div>
             </div>
         </div>
