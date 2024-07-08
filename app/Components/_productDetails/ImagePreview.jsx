26945f42ad89a0c636c38c5b706f7a87b1aa0555
wpseemol <wpseemol@gmail.com>
2024-07-09 00:58:04 +0600

    set fremar mosoan animation on my site and image fixed width and height

--- app/Components/_productDetails/ImagePreview.jsx
+++ app/Components/_productDetails/ImagePreview.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import debounce from '@/utils/debounce';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { useState } from 'react';
 
@@ -11,20 +12,30 @@ export default function ImagePreview({ allImage, productName }) {
         setPreviewImage(img);
     }
 
-    const debouncedHandleImageHover = debounce(handelImagePreviewHover, 250);
+    const debouncedHandleImageHover = debounce(handelImagePreviewHover, 350);
 
     return (
         <div className=" h-fit">
             {/* preview image */}
-            <figure className="max-h-[35rem] overflow-hidden object-cover">
-                <Image
-                    src={previewImage?.imgUrl}
-                    alt={productName}
-                    width={750}
-                    height={550}
-                    className="w-auto h-auto "
-                />
+
+            <figure className="h-[34.4rem]  border border-neutral-500/10 bg-slate-200/10 rounded overflow-hidden object-cover flex justify-center items-center">
+                <motion.div
+                    key={previewImage?.id}
+                    initial={{ y: 30, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: -30, opacity: 0 }}
+                    transition={{ duration: 0.2 }}
+                    className="w-auto h-auto">
+                    <Image
+                        src={previewImage?.imgUrl}
+                        alt={productName}
+                        width={750}
+                        height={550}
+                        className="w-auto h-auto"
+                    />
+                </motion.div>
             </figure>
+
             <div className="flex items-center justify-center gap-4 mt-4">
                 {allImage?.map((img, inx) => (
                     <div
@@ -36,7 +47,7 @@ export default function ImagePreview({ allImage, productName }) {
                             previewImage?.id === img?.id
                                 ? 'border-2 border-green-400'
                                 : 'border-neutral-500'
-                        } h-[3.3rem] w-20 overflow-hidden border rounded flex items-center justify-start`}>
+                        } h-[3.3rem] w-20 overflow-hidden border rounded flex items-center justify-start duration-150`}>
                         <Image
                             src={img?.imgUrl}
                             alt={`${productName} - ${inx + 1}`}
