de4b8e89a2db9b4b8d146ba813e90e4d130085a0
wpseemol <wpseemol@gmail.com>
2024-07-27 00:12:28 +0600

    tab view component

---
+++ app/(adimOrCreator)/dashboard/(subDashboad)/add-product/add-category/page.js
@@ -0,0 +1,10 @@
+export default function AddCategoryPage() {
+    return (
+        <>
+            <h1 className="text-3xl font-semibold ml-5 mb-5">Add category</h1>
+            <section>
+                <p>Add category page</p>
+            </section>
+        </>
+    );
+}
--- app/(adimOrCreator)/dashboard/(subDashboad)/add-product/page.js
+++ app/(adimOrCreator)/dashboard/(subDashboad)/add-product/page.js
@@ -2,8 +2,9 @@ import AddProductSection from './_component/add-product-section';
 
 export default function AddProductPage() {
     return (
-        <main>
+        <>
+            <h1 className="text-3xl font-semibold ml-5 mb-5">Add product</h1>
             <AddProductSection />
-        </main>
+        </>
     );
 }
