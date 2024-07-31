25b2660f560a70de516b6a0e1c76f3b7b1e7db17
wpseemol <wpseemol@gmail.com>
2024-07-31 13:57:47 +0600

    animason dev element remove after animaiton and again created then animate

--- utils/animation.js
+++ utils/animation.js
@@ -14,28 +14,54 @@ export function animationPageIn() {
         }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
             yPercent: 100,
             stagger: 0.15,
+            onComplete: () => {
+                bannerOne.remove();
+                bannerTwo.remove();
+                bannerThree.remove();
+                bannerFour.remove();
+            },
         });
     }
 }
 
 export function animatePageOut(href, router) {
-    const bannerOne = document.getElementById('banner-1');
-    const bannerTwo = document.getElementById('banner-2');
-    const bannerThree = document.getElementById('banner-3');
-    const bannerFour = document.getElementById('banner-4');
+    const banners = [
+        createBanner('left-0', 'banner-1'),
+        createBanner('left-1/4', 'banner-2'),
+        createBanner('left-2/4', 'banner-3'),
+        createBanner('left-3/4', 'banner-4'),
+    ];
 
-    if (bannerOne && bannerTwo && bannerThree && bannerFour) {
-        const tl = gsap.timeline();
+    banners.forEach((banner) => {
+        document.body.appendChild(banner);
+    });
 
-        tl.set([bannerOne, bannerTwo, bannerThree, bannerFour], {
-            yPercent: -100,
-        }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
-            yPercent: 0,
-            stagger: 0.15,
-            onComplete: () => {
-                router.push(href);
-                animationPageIn();
-            },
-        });
-    }
+    const tl = gsap.timeline();
+
+    tl.set(banners, {
+        yPercent: -100,
+    }).to(banners, {
+        yPercent: 0,
+        stagger: 0.15,
+        onComplete: () => {
+            router.push(href);
+            animationPageIn();
+        },
+    });
+}
+
+function createBanner(className = '', idName) {
+    const banner = document.createElement('div');
+    banner.id = idName;
+    banner.classList.add(
+        className,
+        'min-h-screen',
+        'bg-neutral-900',
+        'z-[100]',
+        'fixed',
+        'top-0',
+        'w-1/4'
+    );
+
+    return banner;
 }
