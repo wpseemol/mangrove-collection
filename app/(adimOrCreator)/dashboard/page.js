b6a552778eeb30cce38d09c9e4dbd09112cb0798
wpseemol <wpseemol@gmail.com>
2024-08-08 21:57:27 +0600

    findng dashbor error

--- app/(adimOrCreator)/dashboard/page.js
+++ app/(adimOrCreator)/dashboard/page.js
@@ -1,15 +1,25 @@
 import { auth } from '@/auth/auth';
 import { ADMIN, CREATOR } from '@/constant-value';
+import { redirect } from 'next/navigation';
 
 export default async function DashboardPage() {
     const section = await auth();
 
     let userTypeCake;
-    if (section?.user?.role === ADMIN) {
-        userTypeCake = 'Admin';
-    } else if (section?.user?.role === CREATOR) {
-        userTypeCake = 'Content Creator';
+
+    switch (section?.user?.role) {
+        case ADMIN:
+            userTypeCake = 'Admin';
+            break;
+        case CREATOR:
+            userTypeCake = 'Content Creator';
+            break;
+
+        default:
+            redirect('/account');
+            return;
     }
+
     return (
         <main>
             <div>
