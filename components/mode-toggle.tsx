07bc90117e9c833205c2785b72d3eb0cefa854e5
wpseemol <wpseemol@gmail.com>
2024-08-10 17:30:43 +0600

    on mode togle hans hook name useTheme return setThuem and thems has Thems name fome proivde there in array theme provider thames array

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -12,7 +12,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 export function ModeToggle() {
-    const { setTheme } = useTheme();
+    const { setTheme, themes } = useTheme();
 
     return (
         <DropdownMenu>
@@ -24,15 +24,13 @@ export function ModeToggle() {
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme('light')}>
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('dark')}>
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('system')}>
-                    System
-                </DropdownMenuItem>
+                {themes.map((theme) => (
+                    <DropdownMenuItem
+                        key={theme}
+                        onClick={() => setTheme(theme)}>
+                        {theme}
+                    </DropdownMenuItem>
+                ))}
             </DropdownMenuContent>
         </DropdownMenu>
     );
