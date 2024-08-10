17ab4f9ce256b87f8eef3af55bce59162c98f246
wpseemol <wpseemol@gmail.com>
2024-08-10 21:09:26 +0600

    right path set on globals css file

--- app/(regularUser)/layout.tsx
+++ app/(regularUser)/layout.tsx
@@ -1,7 +1,7 @@
+import '@/app/globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
