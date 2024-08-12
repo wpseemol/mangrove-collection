437e6557f8b1dafbcbd5c6a46c17a80cb1f60bb7
wpseemol <wpseemol@gmail.com>
2024-08-12 12:39:39 +0600

    remove test page drower

--- app/(user)/test/page.tsx
+++ app/(user)/test/page.tsx
@@ -1,10 +1,7 @@
-import { DrawerDemo } from './_components/drower';
-
 export default function TestPage() {
     return (
         <div>
             <p> this is tes page</p>
-            <DrawerDemo />
         </div>
     );
 }
