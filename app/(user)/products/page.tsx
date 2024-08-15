b5850cc7fef728c7009f8178e7742d2a2c000ecf
wpseemol <wpseemol@gmail.com>
2024-08-15 15:35:31 +0600

    bread-crumb file created

--- app/(user)/products/page.tsx
+++ app/(user)/products/page.tsx
@@ -1,6 +1,13 @@
-export default function ProductPage() {
+import Breadcrumb from '@/components/bread-crumb';
+
+export const metadata = {
+    title: 'Mangrove Collection | Products',
+};
+
+export default function ProductPage({ searchParams }) {
     return (
-        <main>
+        <main className="container mx-auto">
+            <Breadcrumb pageName="Products" />
             <p>product page</p>
         </main>
     );
