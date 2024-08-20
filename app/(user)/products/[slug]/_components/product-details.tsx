b0db1f211404a3dd2f68cd63e722b31f0e9351c4
wpseemol <wpseemol@gmail.com>
2024-08-20 15:31:11 +0600

    brendcrumb component use on my product page

--- app/(user)/products/[slug]/_components/product-details.tsx
+++ app/(user)/products/[slug]/_components/product-details.tsx
@@ -1,4 +1,8 @@
+import Breadcrumb from '@/components/bread-crumb';
 import { ProductDetailsType } from '@/types/products';
+import wordEllipsis from '@/utils/word-ellipsis';
+import Link from 'next/link';
+import { FaChevronRight } from 'react-icons/fa6';
 
 export default function ProductDetails({
     details,
@@ -6,9 +10,23 @@ export default function ProductDetails({
     details: ProductDetailsType;
 }) {
     return (
-        <div>
+        <>
+            <Breadcrumb
+                pageName="Products"
+                href="/products"
+                more={
+                    <>
+                        <FaChevronRight className="text-sm text-gray-400" />
+                        <Link href="#">
+                            <p className="text-primary text-base hover:text-primary-foreground duration-150 capitalize">
+                                {wordEllipsis(details.name.toLowerCase())}
+                            </p>
+                        </Link>
+                    </>
+                }
+            />
             <h2 className="text-4xl">{details.name}</h2>
             <pre className="text-wrap">{JSON.stringify(details)}</pre>
-        </div>
+        </>
     );
 }
