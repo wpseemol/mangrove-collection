1b8ececa343dacbfda07eaf4cd9c2e4909cda789
wpseemol <wpseemol@gmail.com>
2024-08-28 03:07:18 +0600

    images file uploasd done

--- app/(adimOrCreator)/dashboard/add-product/_components/media.tsx
+++ app/(adimOrCreator)/dashboard/add-product/_components/media.tsx
@@ -1,19 +1,18 @@
 'use client';
 
 import { AddProductFormType } from '@/types/add-product';
-import { useState } from 'react';
+import Images from './media/images';
 import Thumbnail from './media/thumbnail';
 
 export default function Media({ form }: { form: AddProductFormType }) {
-    const [previewImage, setPreviewImage] = useState(null);
-    const [uploadProgress, setUploadProgress] = useState(0);
-
     return (
         <>
             <div className="mb-4">
                 <Thumbnail form={form} />
             </div>
-            <div className="mb-4">{/* <Images form={form} /> */}</div>
+            <div className="mb-4">
+                <Images form={form} />
+            </div>
         </>
     );
 }
