f61bf4abcbfd7e3fecacd8fd258b97d9ec53bae7
wpseemol <wpseemol@gmail.com>
2024-08-30 17:28:02 +0600

    add category api session or admin or creator chake doen

--- app/api/v1/category/route.ts
+++ app/api/v1/category/route.ts
@@ -1,4 +1,6 @@
+import { auth } from '@/auth/auth';
 import { connectMongoDB } from '@/db/connections/mongoose-connect';
+import { ADMIN, CREATOR } from '@/lib/constant-value';
 import { Category } from '@/lib/schemas/mongoose/category';
 import { CategoryWithMongo_Id } from '@/types/mongoose-models';
 import { NextRequest, NextResponse } from 'next/server';
@@ -35,4 +37,39 @@ export async function GET(required: NextRequest) {
             { status: 500 }
         );
     }
+}
+
+// post or add category
+
+export async function POST(request: NextRequest) {
+    try {
+        const session = await auth();
+        const body = await request.json();
+
+        if (!session) {
+            return NextResponse.json(
+                { error: 'Unauthorized' },
+                { status: 401 }
+            );
+        }
+
+        const isAdmin = !(session?.user?.role === ADMIN);
+        const isCreator = !(session?.user?.role === CREATOR);
+
+        if (!isAdmin && !isCreator) {
+            return NextResponse.json(
+                {
+                    error: 'Forbidden: You do not have permission to add Category.',
+                },
+                { status: 403 }
+            );
+        }
+
+        if (!body) {
+            return NextResponse.json(
+                { error: 'Body category info required.' },
+                { status: 400 }
+            );
+        }
+    } catch (error) {}
 }
