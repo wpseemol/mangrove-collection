901122e4ad7ca42f9fe200e2f00f7e6cffefe3cf
wpseemol <wpseemol@gmail.com>
2024-08-31 01:52:27 +0600

    user table data

--- app/(adimOrCreator)/dashboard/users/page.tsx
+++ app/(adimOrCreator)/dashboard/users/page.tsx
@@ -1,3 +1,9 @@
+import { DataTableDemo } from './_component/user-table';
+
 export default function UserPage() {
-    return <div>user page</div>;
+    return (
+        <section className="container mx-auto md:py-10 py-5">
+            <DataTableDemo />
+        </section>
+    );
 }
