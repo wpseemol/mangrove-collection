3b9aa9ee61ef869fe7b8d712d8bb7058181f44b9
wpseemol <wpseemol@gmail.com>
2024-08-31 02:02:39 +0600

    users page data tabel set created

--- app/(adimOrCreator)/dashboard/users/page.tsx
+++ app/(adimOrCreator)/dashboard/users/page.tsx
@@ -2,7 +2,8 @@ import { DataTableDemo } from './_component/user-table';
 
 export default function UserPage() {
     return (
-        <section className="container mx-auto md:py-10 py-5">
+        <section className="container mx-auto md:my-6 my-5">
+            <h2 className="md:text-2xl text-xl font-semibold">Manage Users</h2>
             <DataTableDemo />
         </section>
     );
