e8688b5dfdf0734c37960fe86d6f3567303c9f89
wpseemol <wpseemol@gmail.com>
2024-08-31 02:03:19 +0600

    user manag account dada add

--- app/(adimOrCreator)/dashboard/users/page.tsx
+++ app/(adimOrCreator)/dashboard/users/page.tsx
@@ -1,6 +1,6 @@
 import { DataTableDemo } from './_component/user-table';
 
-export default function UserPage() {
+export default function UsersPage() {
     return (
         <section className="container mx-auto md:my-6 my-5">
             <h2 className="md:text-2xl text-xl font-semibold">Manage Users</h2>
