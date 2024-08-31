1ea854c139b86b51235dfc3167451fef2018ebcd
wpseemol <wpseemol@gmail.com>
2024-09-01 01:47:09 +0600

    try to get outher waya to pass cookies

--- app/(adimOrCreator)/dashboard/users/page.tsx
+++ app/(adimOrCreator)/dashboard/users/page.tsx
@@ -5,6 +5,8 @@ import { DataTable } from './_component/data-table';
 export default async function UsersPage() {
     const allUser = await getUsers();
 
+    console.log(allUser);
+
     return (
         <section className="container mx-auto md:my-6 my-5 md:p-0 p-2">
             <h2 className="md:text-2xl text-xl font-semibold">Manage Users</h2>
