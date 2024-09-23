64c2a676a4479b0351a25d76e2a2e2a33cd8facb
wpseemol <wpseemol@gmail.com>
2024-09-23 12:38:53 +0600

    cart product sumari order

--- app/(user)/cart/_components/cart-order-summary.tsx
+++ app/(user)/cart/_components/cart-order-summary.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
 import { useCart } from '@/hooks';
 import { CartProductType, OrderSummary } from '@/types/cart';
 
@@ -13,11 +14,17 @@ export default function CartOrderSummary() {
         orderProducts.some((item) => item.slug === product.slug)
     ) as CartProductType[];
 
-    const totalPrice = orderProduct.reduce((acc, curr) => {
+    const subTotal = orderProduct.reduce((acc, curr) => {
         const multiplyPrice = curr.price * curr.quantity;
 
         return acc + multiplyPrice;
     }, 0);
+
+    const shippingFee = 0;
+
+    const voucherCode = 0;
+
+    const totalPrice = subTotal + shippingFee + voucherCode;
 
     const totalCount = orderProduct.reduce((acc, curr) => {
         const count = curr.quantity + acc;
@@ -29,7 +36,7 @@ export default function CartOrderSummary() {
             <CardHeader className="p-0 font-medium">Order Summary</CardHeader>
             <CardContent className="px-0">
                 {/* cart price section */}
-                <div className="flex justify-between items-start">
+                <div className="flex justify-between items-start my-3">
                     <p>
                         Subtotal (
                         {orderSummary.length > 1
@@ -37,12 +44,30 @@ export default function CartOrderSummary() {
                             : orderSummary.length + ' item'}
                         )
                     </p>{' '}
-                    <span>{totalPrice.toFixed(2)}&#2547;</span>
+                    <span>{subTotal.toFixed(2)}&#2547;</span>
+                </div>
+                {/* cart price section */}
+
+                <div className="flex justify-between items-start my-3">
+                    <p>Shipping Fee</p>{' '}
+                    <span>{shippingFee.toFixed(2)}&#2547;</span>
+                </div>
+
+                {/* voucher code apply */}
+                <div className="flex items-center gap-2 mb-3">
+                    <Input type="text" placeholder="Enter Voucher Code" />{' '}
+                    <Button>APPLY</Button>
+                </div>
+                {/* voucher code apply */}
+
+                {/* cart price section */}
+                <div className="flex justify-between items-start mb-3">
+                    <p>Total</p> <span>{totalPrice.toFixed(2)}&#2547;</span>
                 </div>
                 {/* cart price section */}
             </CardContent>
 
-            <Button className="w-full">Proceed to Pay {totalCount}</Button>
+            <Button className="w-full">Proceed to Pay ({totalCount})</Button>
         </Card>
     ) : (
         <Card>
