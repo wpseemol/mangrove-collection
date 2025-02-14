309b4b5f6e0e3e740a11f67582352073f059a638
Seemol Chakroborti <wpseemol>
2025-02-14 12:52:58 +0600

    feacher section make separet section

--- src/app/(user)/_components/home/hero-section.tsx
+++ src/app/(user)/_components/home/hero-section.tsx
@@ -1,14 +1,9 @@
-import { Card } from '@/components/ui/card';
-import { Feature } from '@/types/home';
 import Image from 'next/image';
-import { BsFilterSquare } from 'react-icons/bs';
-import { ImWhatsapp } from 'react-icons/im';
-import { MdOutlineMessage } from 'react-icons/md';
 import HeroSlickSlider from './hero-slick-slider';
 
 export default function HeroSection() {
     return (
-        <section className=" container mx-auto flex flex-col justify-center md:py-10 pb-5 ">
+        <section className=" container mx-auto flex flex-col justify-center 2xl:pt-10 md:pt-8 pb-5 sm:pb-0 ">
             {/* hero slider section */}
             <div className="grid grid-cols-2 lg:grid-cols-10 gap-3 lg:gap-6 p-3 md:p-0 2xl:h-[515px] lg:h-[450px] ">
                 {/* 2xl:h-[515px] lg:h-[450px] */}
@@ -42,54 +37,6 @@ export default function HeroSection() {
                 </div>
             </div>
             {/* hero slider section */}
-
-            <div
-                className={`${
-                    otherInfo.length > 3
-                        ? 'grid md:grid-cols-4 grid-cols-2'
-                        : 'flex justify-center flex-wrap'
-                }  md:mt-10 mt-5  md:gap-14 gap-5 p-2`}>
-                {otherInfo.map((info) => (
-                    <Card
-                        key={info.id}
-                        className="bg-card px-5 py-2 rounded-sm">
-                        <div className="flex gap-4 items-center">
-                            <div className="w-12 h-12 rounded-full bg-primary text-neutral-200 flex justify-center items-center text-xl">
-                                {info.icon}
-                            </div>
-                            <div>
-                                <h2 className="text-secondary-foreground font-medium">
-                                    {info.title}
-                                </h2>
-                                <p className="text-muted-foreground">
-                                    {info.description}
-                                </p>
-                            </div>
-                        </div>
-                    </Card>
-                ))}
-            </div>
         </section>
     );
 }
-
-const otherInfo: Feature[] = [
-    {
-        id: '1',
-        icon: <BsFilterSquare />,
-        title: 'Outfit Finder',
-        description: 'Find Outfit for product',
-    },
-    {
-        id: '2',
-        icon: <MdOutlineMessage />,
-        title: 'Share Experience',
-        description: 'We value you feedback',
-    },
-    {
-        id: '3',
-        icon: <ImWhatsapp />,
-        title: 'Online Support',
-        description: 'Get support on WhatsApp',
-    },
-];
--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,9 +1,11 @@
+import FeatureSection from './_components/home/feature-section';
 import HeroSection from './_components/home/hero-section';
 
 export default function HomePage() {
     return (
         <main>
             <HeroSection />
+            <FeatureSection />
         </main>
     );
 }
