5343bd3b413c2ad11acedf17fd5fa486acecc240
Seemol Chakroborti <wpseemol>
2025-02-18 12:23:22 +0600

    mongodb product schema write

---
+++ src/types/mongoose/product/index.d.ts
@@ -0,0 +1,41 @@
+import { Types } from 'mongoose';
+
+interface Price {
+    variantId: string;
+    price: number;
+    select: boolean;
+}
+
+interface Image {
+    id: string;
+    imgUrl: string;
+}
+
+interface Variant {
+    id: string;
+    type: string;
+    title: string;
+}
+
+export interface Product {
+    _id?: Types.ObjectId;
+    name: string;
+    category: Types.ObjectId;
+    slug: string;
+    unit: string;
+    size?: string;
+    price: Price[];
+    currency: string;
+    offer?: number;
+    shortDescription?: string;
+    thumbnail: string;
+    description: string;
+    images: Image[];
+    variants: Variant[];
+    author: Types.ObjectId;
+    createdAt?: Date;
+    rating?: Types.ObjectId;
+    comment?: Types.ObjectId;
+    popularity?: number;
+    tags?: string[];
+}
