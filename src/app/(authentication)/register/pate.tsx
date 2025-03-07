55b13e8088ebd0c4fc2c82cdac119119768a7ab2
Seemol Chakroborti <wpseemol>
2025-03-07 17:16:05 +0600

    register page create

---
+++ src/app/(authentication)/register/pate.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+    return (
+        <main className="flex items-center justify-center sm:p-8 py-8 min-h-screen">
+            register page
+        </main>
+    );
+}
