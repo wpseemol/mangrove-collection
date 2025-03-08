89bfda4e7a4642b4a25d7497fcd0c839401226a4
Seemol Chakroborti <wpseemol>
2025-03-08 12:51:21 +0600

    login user fetch call ready

---
+++ src/server/login/index.ts
@@ -0,0 +1,38 @@
+'use server';
+
+const baseUrl = process.env.NEXT_PUBLIC_API_URL!;
+
+export async function userLogin(
+    loginCredential: LoginCredentialType
+): Promise<LoginUserDetailsType> {
+    const response = await fetch(`${baseUrl}api/v1/user/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginCredential),
+    });
+
+    const isRegister = await response.json();
+
+    if (response.ok) {
+        return {
+            message: isRegister?.message,
+            redirect: true,
+        };
+    }
+
+    return {
+        message: isRegister?.message,
+        redirect: false,
+    };
+}
+
+interface LoginCredentialType {
+    email: string;
+    password: string;
+}
+
+interface LoginUserDetailsType {
+    id: string;
+}
