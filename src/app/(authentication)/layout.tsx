4bdaa9f68857b6cf61beb313604170f0c177706f
Seemol Chakroborti <wpseemol>
2025-03-09 16:08:41 +0600

    auth secion ready

--- src/app/(authentication)/layout.tsx
+++ src/app/(authentication)/layout.tsx
@@ -1,5 +1,5 @@
 import { auth } from '@/auth';
-import { redirect } from 'next/navigation';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 export default async function AuthenticationLayout({
@@ -13,6 +13,6 @@ export default async function AuthenticationLayout({
         return <>{children}</>;
     }
 
-    redirect('/');
+    notFound();
     return <></>;
 }
