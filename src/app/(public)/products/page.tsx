db5853059c7ba980f3f29a96ce0990aa7536dbc5
Seemol Chakroborti <wpseemol>
2025-04-23 00:26:16 +0600

    tab endent update 4 to 5

--- src/app/(public)/products/page.tsx
+++ src/app/(public)/products/page.tsx
@@ -1,50 +1,50 @@
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
+     Breadcrumb,
+     BreadcrumbItem,
+     BreadcrumbLink,
+     BreadcrumbList,
+     BreadcrumbPage,
+     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { getCategoryWithCount } from "@/lib/server/category-with-count";
 import FilterSection from "./_components/filter-section";
 import ProductSection from "./_components/products-section";
 
 export default async function ProductsPage({
-    searchParams,
+     searchParams,
 }: {
-    searchParams: Promise<SearchParamsType>;
+     searchParams: Promise<SearchParamsType>;
 }) {
-    const categoryWithCount = await getCategoryWithCount();
+     const categoryWithCount = await getCategoryWithCount();
 
-    const searchParamsData = await searchParams;
+     const searchParamsData = await searchParams;
 
-    return (
-        <main className="container mx-auto">
-            {/* breadcrumb Product page*/}
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>Products</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
-            {/* breadcrumb Product page*/}
+     return (
+          <main className="container mx-auto">
+               {/* breadcrumb Product page*/}
+               <Breadcrumb>
+                    <BreadcrumbList>
+                         <BreadcrumbItem>
+                              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                         </BreadcrumbItem>
+                         <BreadcrumbSeparator />
+                         <BreadcrumbItem>
+                              <BreadcrumbPage>Products</BreadcrumbPage>
+                         </BreadcrumbItem>
+                    </BreadcrumbList>
+               </Breadcrumb>
+               {/* breadcrumb Product page*/}
 
-            <section className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-1 gap-6 pt-4 pb-16 items-start justify-center sm:mx-auto mx-2 ">
-                <FilterSection categoryWithCount={categoryWithCount} />
-                <ProductSection searchParamsData={searchParamsData} />
-            </section>
-        </main>
-    );
+               <section className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-1 gap-6 pt-4 pb-16 items-start justify-center sm:mx-auto mx-2 ">
+                    <FilterSection categoryWithCount={categoryWithCount} />
+                    <ProductSection searchParamsData={searchParamsData} />
+               </section>
+          </main>
+     );
 }
 
 export interface SearchParamsType {
-    category: string;
-    price: string;
-    size: string;
+     category: string;
+     price: string;
+     size: string;
 }
