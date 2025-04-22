77d49dea86b035e03851f7823802d85c80e94da6
Seemol Chakroborti <wpseemol>
2025-04-23 00:19:37 +0600

    setting update

--- src/app/(public)/products/page.tsx
+++ src/app/(public)/products/page.tsx
@@ -1,41 +1,50 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
-import FilterSection from "./_components/filter-section";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { getCategoryWithCount } from "@/lib/server/category-with-count";
+import FilterSection from "./_components/filter-section";
 import ProductSection from "./_components/products-section";
-import { use } from "react";
 
-export default async function ProductsPage({searchParams}:{searchParams:Promise<SearchParamsType>}) {
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParamsType>;
+}) {
+  const categoryWithCount = await getCategoryWithCount();
 
-    const categoryWithCount = await getCategoryWithCount();
+  const searchParamsData = await searchParams;
 
-    const searchParamsData = await searchParams;
+  return (
+    <main className="container mx-auto">
+      {/* breadcrumb Product page*/}
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Products</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* breadcrumb Product page*/}
 
-    
-    
+      <section className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-1 gap-6 pt-4 pb-16 items-start justify-center sm:mx-auto mx-2 ">
+        <FilterSection categoryWithCount={categoryWithCount} />
+        <ProductSection searchParamsData={searchParamsData} />
+      </section>
+    </main>
+  );
+}
 
-    return (
-        <main className="container mx-auto">
-            {/* breadcrumb Product page*/}
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>Products</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
-            {/* breadcrumb Product page*/}
-
-            <section className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-1 gap-6 pt-4 pb-16 items-start justify-center sm:mx-auto mx-2 ">
-                <FilterSection categoryWithCount={categoryWithCount} />
-                <ProductSection searchParamsData={searchParamsData} />
-            </section>
-        </main>
-    );
-};
-
-
-export interface SearchParamsType {category:string;price:string;size:string}
+export interface SearchParamsType {
+  category: string;
+  price: string;
+  size: string;
+}
