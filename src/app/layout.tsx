0654e65f544ef18b9118ab903789d3909f03a0c5
wpseemol <wpseemol@gmail.com>
2025-07-15 21:46:35 +0600

    new pageage install name nextjs-toploader for top loder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,28 @@
-import type { Metadata } from 'next';
-import './globals.css';
-
-import { Poppins, Roboto } from 'next/font/google';
+import type { Metadata } from "next";
+import { Poppins, Roboto } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
+import "./globals.css";
 
 const poppins = Poppins({
-    subsets: ['latin'],
-    weight: ['400', '500', '600', '700'],
+     subsets: ["latin"],
+     weight: ["400", "500", "600", "700"],
 });
-const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata: Metadata = {
-    title: 'Mangrove Collection',
+     title: "Mangrove Collection",
 };
 export default function RootLayout({
-    children,
+     children,
 }: Readonly<{
-    children: React.ReactNode;
+     children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning className='scroll-smooth'>
-            <body
-                className={`${poppins.className} ${roboto.className}`}
-                >
-                {children}
-            </body>
-        </html>
-    );
+     return (
+          <html lang="en" suppressHydrationWarning className="scroll-smooth">
+               <body className={`${poppins.className} ${roboto.className}`}>
+                    <NextTopLoader color="green" />
+                    {children}
+               </body>
+          </html>
+     );
 }
