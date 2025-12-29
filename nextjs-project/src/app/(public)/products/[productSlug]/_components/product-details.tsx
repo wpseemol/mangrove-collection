42fbd3b88f63aa880c1f78f65aea8418feead6f1
wpseemol <wpseemol@gmail.com>
2025-12-29 20:50:24 +0600

    product details page mobile view update

--- nextjs-project/src/app/(public)/products/[productSlug]/_components/product-details.tsx
+++ nextjs-project/src/app/(public)/products/[productSlug]/_components/product-details.tsx
@@ -11,78 +11,77 @@ import SelectedVariant from "./selected-variant";
 import SocialShareBtn from "./social-share-btn";
 
 export default function ProductDetails({
-     details,
+    details,
 }: {
-     details: ProductDetailsType;
+    details: ProductDetailsType;
 }) {
-     const displayPrice = details.price.find((item) => item.select);
+    const displayPrice = details.price.find((item) => item.select);
 
-     const displayVariant = Array.from(
-          new Set(details.variants.map((item) => item.type))
-     );
+    const displayVariant = Array.from(
+        new Set(details.variants.map((item) => item.type))
+    );
 
-     return (
-          <section>
-               <div
-                    key={details.id}
-                    className="flex md:flex-nowrap justify-center flex-wrap gap-6 group relative md:text-start text-center md:mx-14 mt-6  "
-               >
-                    {/* image preview component */}
-                    {/* allImage function return array and input a single image url and images array */}
-                    <ProductImages
-                         allImage={JSON.stringify(
-                              allImageArray(
-                                   details.thumbnail ||
-                                        "/assets/logo/no-image.jpg",
-                                   details.images
-                              )
-                         )}
-                         productName={details.name}
-                    />
-                    <div className="flex flex-col gap-y-3 w-full ">
-                         <h2 className="md:text-3xl text-xl font-medium capitalize group-hover:underline group-hover:underline-offset-4 group-hover:text-primary-foreground duration-200">
-                              {details.name}
-                         </h2>
+    return (
+        <section>
+            <div
+                key={details.id}
+                className="flex md:flex-nowrap justify-center flex-wrap gap-6 group relative md:text-start text-center md:mx-14 mt-6  "
+            >
+                {/* image preview component */}
+                {/* allImage function return array and input a single image url and images array */}
+                <ProductImages
+                    allImage={JSON.stringify(
+                        allImageArray(
+                            details.thumbnail || "/assets/logo/no-image.jpg",
+                            details.images
+                        )
+                    )}
+                    productName={details.name}
+                />
+                <div className="flex flex-col gap-y-3 w-full ">
+                    <h2 className="md:text-3xl text-xl font-medium capitalize group-hover:underline group-hover:underline-offset-4 group-hover:text-primary-foreground duration-200">
+                        {details.name}
+                    </h2>
 
-                         {/* price section */}
-                         <div className="flex md:justify-start justify-center items-baseline font-roboto">
-                              <p className="text-xl text-primary font-semibold">
-                                   {displayPrice && (
-                                        <PriceSection
-                                             displayPrice={displayPrice.price}
-                                             priceVariants={JSON.stringify(
-                                                  details.price
-                                             )}
-                                        />
-                                   )}
-                                   <CurrencyIcon currency={details.currency} />{" "}
-                              </p>
+                    {/* price section */}
+                    <div className="flex md:justify-start justify-center items-baseline font-roboto">
+                        <p className="text-xl text-primary font-semibold">
+                            {displayPrice && (
+                                <PriceSection
+                                    displayPrice={displayPrice.price}
+                                    priceVariants={JSON.stringify(
+                                        details.price
+                                    )}
+                                />
+                            )}
+                            <CurrencyIcon currency={details.currency} />{" "}
+                        </p>
 
-                              {/*
+                        {/*
                             offer price
                         <p className="text-base text-gray-400 line-through">
                             ${productDetails?.price?.toFixed(2)}
                         </p> */}
-                         </div>
-                         {/* price section */}
+                    </div>
+                    {/* price section */}
 
-                         {/* category section */}
-                         <p className="">
-                              <span className="text-gray-800 font-semibold">
-                                   Category :{" "}
-                              </span>
+                    {/* category section */}
+                    <p className="">
+                        <span className="text-gray-800 font-semibold">
+                            Category :{" "}
+                        </span>
 
-                              <Link
-                                   href={`/products?category=${details.category.slug}`}
-                              >
-                                   <span className="text-gray-600 capitalize">
-                                        {details.category.name.toLowerCase()}
-                                   </span>
-                              </Link>
-                         </p>
-                         {/* category section */}
+                        <Link
+                            href={`/products?category=${details.category.slug}`}
+                        >
+                            <span className="text-gray-600 capitalize">
+                                {details.category.name.toLowerCase()}
+                            </span>
+                        </Link>
+                    </p>
+                    {/* category section */}
 
-                         {/* <div className="flex items-center mb-4">
+                    {/* <div className="flex items-center mb-4">
                               <div
                                    className="flex gap-1 text-sm text-yellow-400"
                                    title={`rating Number ${productDetails?.rating}`}
@@ -98,33 +97,29 @@ export default function ProductDetails({
                               </div>
                          </div> */}
 
-                         <p className="text-gray-800 font-semibold space-x-2">
-                              <span>Unit:</span>
-                              <span className="text-green-600">
-                                   {details.unit}{" "}
-                              </span>
-                         </p>
-                         {/* variant section */}
-                         {displayVariant?.length > 0 &&
-                              displayVariant?.map((variant) => (
-                                   <p key={variant} className="space-x-2">
-                                        <span className="text-gray-800 font-semibold capitalize">
-                                             {variant}:
-                                        </span>
-                                        <SelectedVariant
-                                             type={variant}
-                                             variants={JSON.stringify(
-                                                  details.variants
-                                             )}
-                                             displayPrice={JSON.stringify(
-                                                  displayPrice as Price
-                                             )}
-                                        />
-                                   </p>
-                              ))}
-                         {/* variant section */}
+                    <p className="text-gray-800 font-semibold space-x-2">
+                        <span>Unit:</span>
+                        <span className="text-green-600">{details.unit} </span>
+                    </p>
+                    {/* variant section */}
+                    {displayVariant?.length > 0 &&
+                        displayVariant?.map((variant) => (
+                            <p key={variant} className="space-x-2">
+                                <span className="text-gray-800 font-semibold capitalize">
+                                    {variant}:
+                                </span>
+                                <SelectedVariant
+                                    type={variant}
+                                    variants={JSON.stringify(details.variants)}
+                                    displayPrice={JSON.stringify(
+                                        displayPrice as Price
+                                    )}
+                                />
+                            </p>
+                        ))}
+                    {/* variant section */}
 
-                         {/* <p className="space-x-2">
+                    {/* <p className="space-x-2">
                             <span className="text-gray-800 font-semibold">
                                 {dictionary?.singleProduct?.sku}:
                             </span>
@@ -133,38 +128,38 @@ export default function ProductDetails({
                             </span>
                         </p> */}
 
-                         <p
-                              className="mt-4 text-gray-600"
-                              dangerouslySetInnerHTML={{
-                                   __html: details.shortDescription || "",
-                              }}
-                         />
+                    <p
+                        className="mt-4 text-gray-600"
+                        dangerouslySetInnerHTML={{
+                            __html: details.shortDescription || "",
+                        }}
+                    />
 
-                         {/* btn click site */}
-                         <div>
-                              <QuantityCounter />
-                         </div>
+                    {/* btn click site */}
+                    <div className="md:mx-0 mx-auto">
+                        <QuantityCounter />
+                    </div>
 
-                         <div className="flex items-center justify-center md:justify-start gap-4 mt-4">
-                              {/* bye button */}
-                              {displayPrice && (
-                                   <DetailsBuyBtn
-                                        productId={details.id}
-                                        selectedPriceId={displayPrice.variantId}
-                                   />
-                              )}
-                              {/* bye button */}
-                              {/* cart button */}
-                              {displayPrice && (
-                                   <DetailsCartBtn
-                                        productId={details.id}
-                                        selectedPriceId={displayPrice.variantId}
-                                   />
-                              )}
-                              {/* cart button */}
-                         </div>
+                    <div className="flex items-center justify-center md:justify-start gap-4 mt-4">
+                        {/* bye button */}
+                        {displayPrice && (
+                            <DetailsBuyBtn
+                                productId={details.id}
+                                selectedPriceId={displayPrice.variantId}
+                            />
+                        )}
+                        {/* bye button */}
+                        {/* cart button */}
+                        {displayPrice && (
+                            <DetailsCartBtn
+                                productId={details.id}
+                                selectedPriceId={displayPrice.variantId}
+                            />
+                        )}
+                        {/* cart button */}
+                    </div>
 
-                         {/* <div className="mt-4">
+                    {/* <div className="mt-4">
                         <h3 className="text-sm text-gray-800 uppercase mb-1">
                             {dictionary?.singleProduct?.quantity}
                         </h3>
@@ -173,69 +168,69 @@ export default function ProductDetails({
                         />
                     </div> */}
 
-                         {/* <div className="mt-6 flex gap-3 border-b border-gray-200 pb-5 pt-5"> */}
-                         {/* single page add to cart btn */}
-                         {/* <SinglePageAddToCart
+                    {/* <div className="mt-6 flex gap-3 border-b border-gray-200 pb-5 pt-5"> */}
+                    {/* single page add to cart btn */}
+                    {/* <SinglePageAddToCart
                             availableQuantity={productDetails?.quantity}
                             loginUserId={loginUserId?.loginUserId}
                             productId={productId}
                         /> */}
-                         {/* wish list btn */}
-                         {/* <WishlistBtnDetailsPage
+                    {/* wish list btn */}
+                    {/* <WishlistBtnDetailsPage
                             wishlistProductIdArray={WishlistProductArray}
                             loginUserId={loginUserId}
                             productId={productId}
                         /> */}
-                         {/* </div> */}
-                         {/* btn click site */}
+                    {/* </div> */}
+                    {/* btn click site */}
 
-                         <SocialShareBtn
-                              title={details.name}
-                              description={details.description}
-                         />
-                    </div>
-               </div>
+                    <SocialShareBtn
+                        title={details.name}
+                        description={details.description}
+                    />
+                </div>
+            </div>
 
-               {/*  description  */}
-               <div className="pb-16 mt-2">
-                    <h3 className="border-b border-gray-200 font-roboto text-gray-800 pb-3 font-medium text-center md:text-left">
-                         Description
-                    </h3>
-                    <div className=" mx-4 pt-6 text-gray-600 md:mx-0 sm:mx-auto">
-                         <div
-                              className="tiptap"
-                              dangerouslySetInnerHTML={{
-                                   __html: details.description,
-                              }}
-                         />
-                    </div>
-               </div>
-               {/* description */}
-          </section>
-     );
+            {/*  description  */}
+            <div className="pb-16 mt-2">
+                <h3 className="border-b border-gray-200 font-roboto text-gray-800 pb-3 font-medium text-center md:text-left">
+                    Description
+                </h3>
+                <div className=" mx-4 pt-6 text-gray-600 md:mx-0 sm:mx-auto">
+                    <div
+                        className="tiptap"
+                        dangerouslySetInnerHTML={{
+                            __html: details.description,
+                        }}
+                    />
+                </div>
+            </div>
+            {/* description */}
+        </section>
+    );
 }
 
 interface ImageType {
-     id: string;
-     imgUrl: string;
+    id: string;
+    imgUrl: string;
 }
 
 export function allImageArray(
-     thumbnail: string,
-     images?: ImageType[]
+    thumbnail: string,
+    images?: ImageType[]
 ): ImageType[] {
-     let allImage: ImageType[] = [];
+    let allImage: ImageType[] = [];
 
-     if (thumbnail) {
-          allImage.push({
-               id: crypto.randomUUID(),
-               imgUrl: thumbnail || "/assets/logo/no-image.jpg",
-          });
-     }
+    if (thumbnail) {
+        allImage.push({
+            id: crypto.randomUUID(),
+            imgUrl: thumbnail || "/assets/logo/no-image.jpg",
+        });
+    }
 
-     if (images) {
-          allImage = [...allImage, ...images];
-     }
+    if (images) {
+        allImage = [...allImage, ...images];
+    }
 
-     return allImage;
+    return allImage;
 }
--- nextjs-project/src/db/home/sliderContent.ts
+++ nextjs-project/src/db/home/sliderContent.ts
@@ -1,14 +1,12 @@
 import { BannerSliderType } from "@/types/home";
 
-
 export const sliderContent: BannerSliderType[] = [
-     {
-          imageUrl: "/assets/image/mangrove-collection (1).jpg",
-          linkStatus: false,
-          linkTarget: "#",
-          title: "Mangrove collection",
-          id: "slide-1",
-          type: "slides"
-     },
-   
+    {
+        imageUrl: "/assets/image/mangrove-collection.jpg",
+        linkStatus: false,
+        linkTarget: "#",
+        title: "Mangrove collection",
+        id: "slide-1",
+        type: "slides",
+    },
 ];
